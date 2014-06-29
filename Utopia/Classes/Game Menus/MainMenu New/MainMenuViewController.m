275c1b0a9db958a151808a60537e3568806e2395
Ashwin Kamath <ashwin@lvl6.com>
2014-06-29 00:56:18 -0700

    starting on main menu

---
+++ Utopia/Classes/Game Menus/MainMenu New/MainMenuViewController.m
@@ -0,0 +1,18 @@
+//
+//  MainMenuViewController.m
+//  Utopia
+//
+//  Created by Ashwin Kamath on 6/29/14.
+//  Copyright (c) 2014 LVL6. All rights reserved.
+//
+
+#import "MainMenuViewController.h"
+
+@interface MainMenuViewController ()
+
+@end
+
+@implementation MainMenuViewController
+
+
+@end
