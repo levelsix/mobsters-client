fc2e1d43890926822d2b6004092a41ca9ca6d059
inlight2 <kenny@lvl6.com>
2015-02-12 15:53:44 -0800

    push request popup

---
+++ Utopia/Classes/GameMenus/PushNotiifcations/RequestPushNotificationViewController.h
@@ -0,0 +1,13 @@
+//
+//  RequestPushNotificationViewController.h
+//  Utopia
+//
+//  Created by Kenneth Cox on 2/11/15.
+//  Copyright (c) 2015 LVL6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RequestPushNotificationViewController : UIViewController
+
+@end
---
+++ Utopia/Classes/GameMenus/PushNotiifcations/RequestPushNotificationViewController.m
@@ -0,0 +1,37 @@
+//
+//  RequestPushNotificationViewController.m
+//  Utopia
+//
+//  Created by Kenneth Cox on 2/11/15.
+//  Copyright (c) 2015 LVL6. All rights reserved.
+//
+
+#import "RequestPushNotificationViewController.h"
+
+@interface RequestPushNotificationViewController ()
+
+@end
+
+@implementation RequestPushNotificationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
