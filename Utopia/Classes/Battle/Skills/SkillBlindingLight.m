c797d5b929d12e4cd6dd78073f4250497c9a2a56
Rob Giusti <rockenstein.rock@gmail.com>
2015-02-19 14:02:08 -0800

    Removed a big pile of logos from a bunch of skills

--- Utopia/Classes/Battle/Skills/SkillBlindingLight.m
+++ Utopia/Classes/Battle/Skills/SkillBlindingLight.m
@@ -204,34 +204,34 @@
 
 - (void) showLogo:(BOOL)showMissedLabel
 {
-  // Display logo
-  CCSprite* logoSprite = [CCSprite spriteWithImageNamed:[self.skillImageNamePrefix stringByAppendingString:kSkillMiniLogoImageNameSuffix]];
-  logoSprite.position = CGPointMake((self.enemySprite.position.x + self.playerSprite.position.x) * .5f + self.playerSprite.contentSize.width * .5f - 10.f,
-                                    (self.playerSprite.position.y + self.enemySprite.position.y) * .5f + self.playerSprite.contentSize.height * .5f);
-  logoSprite.scale = 0.f;
-  [self.playerSprite.parent addChild:logoSprite z:50];
-  
-  if (showMissedLabel)
-  {
-    // Display missed label
-    CCLabelTTF* floatingLabel = [CCLabelTTF labelWithString:@"MISSED" fontName:@"GothamNarrow-Ultra" fontSize:12];
-    floatingLabel.position = ccp(logoSprite.spriteFrame.rect.size.width * .5f, -13.f);
-    floatingLabel.fontColor = [CCColor colorWithRed:255.f / 225.f green:44.f / 225.f blue:44.f / 225.f];
-    floatingLabel.outlineColor = [CCColor whiteColor];
-    floatingLabel.shadowOffset = ccp(0.f, -1.f);
-    floatingLabel.shadowColor = [CCColor colorWithWhite:0.f alpha:.75f];
-    floatingLabel.shadowBlurRadius = 2.f;
-    [logoSprite addChild:floatingLabel];
-  }
-  
-  // Animate
-  [logoSprite runAction:[CCActionSequence actions:
-                         [CCActionDelay actionWithDuration:.3f],
-                         [CCActionEaseBounceOut actionWithAction:[CCActionScaleTo actionWithDuration:.5f scale:1.f]],
-                         [CCActionDelay actionWithDuration:.5f],
-                         [CCActionEaseIn actionWithAction:[CCActionScaleTo actionWithDuration:.3f scale:0.f]],
-                         [CCActionRemove action],
-                         nil]];
+//  // Display logo
+//  CCSprite* logoSprite = [CCSprite spriteWithImageNamed:[self.skillImageNamePrefix stringByAppendingString:kSkillMiniLogoImageNameSuffix]];
+//  logoSprite.position = CGPointMake((self.enemySprite.position.x + self.playerSprite.position.x) * .5f + self.playerSprite.contentSize.width * .5f - 10.f,
+//                                    (self.playerSprite.position.y + self.enemySprite.position.y) * .5f + self.playerSprite.contentSize.height * .5f);
+//  logoSprite.scale = 0.f;
+//  [self.playerSprite.parent addChild:logoSprite z:50];
+//  
+//  if (showMissedLabel)
+//  {
+//    // Display missed label
+//    CCLabelTTF* floatingLabel = [CCLabelTTF labelWithString:@"MISSED" fontName:@"GothamNarrow-Ultra" fontSize:12];
+//    floatingLabel.position = ccp(logoSprite.spriteFrame.rect.size.width * .5f, -13.f);
+//    floatingLabel.fontColor = [CCColor colorWithRed:255.f / 225.f green:44.f / 225.f blue:44.f / 225.f];
+//    floatingLabel.outlineColor = [CCColor whiteColor];
+//    floatingLabel.shadowOffset = ccp(0.f, -1.f);
+//    floatingLabel.shadowColor = [CCColor colorWithWhite:0.f alpha:.75f];
+//    floatingLabel.shadowBlurRadius = 2.f;
+//    [logoSprite addChild:floatingLabel];
+//  }
+//  
+//  // Animate
+//  [logoSprite runAction:[CCActionSequence actions:
+//                         [CCActionDelay actionWithDuration:.3f],
+//                         [CCActionEaseBounceOut actionWithAction:[CCActionScaleTo actionWithDuration:.5f scale:1.f]],
+//                         [CCActionDelay actionWithDuration:.5f],
+//                         [CCActionEaseIn actionWithAction:[CCActionScaleTo actionWithDuration:.3f scale:0.f]],
+//                         [CCActionRemove action],
+//                         nil]];
 }
 
 @end
