9b4cc36af6f0ae55b8c43e4988a8c089b46db80a
Rob Giusti <rockenstein.rock@gmail.com>
2015-02-23 15:09:30 -0800

    Skewer/Double-Slap simplified by inheriting from Quick Attack

--- Utopia/Classes/Battle/Skills/SkillSkewer.m
+++ Utopia/Classes/Battle/Skills/SkillSkewer.m
@@ -2,6 +2,11 @@
 //  SkillSkewer
 //  Utopia
 //
+//  NOTE: This skill is actually "Double Slap"
+//  It was originally spec'd as "Skewer," and then
+//  "Poison Skewer" wound up getting called skewer and everything
+//  became a goddamn mess.
+//
 //  Created by Rob Giusti on 1/26/15
 //  Copyright (c) 2015 LVL6. All rights reserved.
 //
@@ -34,66 +39,13 @@
 
 #pragma mark - Overrides
 
-- (BOOL) skillCalledWithTrigger:(SkillTriggerPoint)trigger execute:(BOOL)execute
-{
-  if ([super skillCalledWithTrigger:trigger execute:execute])
-    return YES;
-  
-  if (trigger == SkillTriggerPointEndOfPlayerMove)
-  {
-    if ([self skillIsReady])
-    {
-      if (execute)
-      {
-        [self.battleLayer.orbLayer.bgdLayer turnTheLightsOff];
-        [self.battleLayer.orbLayer disallowInput];
-        [self showSkillPopupOverlay:YES withCompletion:^(){
-          [self dealSkewer];
-        }];
-      }
-      return YES;
-    }
-  }
-  
-  return NO;
-}
-
-#pragma mark - Skill logic
-
-- (void) dealSkewer
-{
-  // Perform attack animation
-  if (self.belongsToPlayer)
-    [self.playerSprite performFarAttackAnimationWithStrength:0.f shouldEvade:NO enemy:self.enemySprite
-                                                      target:self selector:@selector(dealSkewer1) animCompletion:nil];
-  else
-    [self.enemySprite performNearAttackAnimationWithEnemy:self.playerSprite shouldReturn:YES shouldEvade:NO shouldFlinch:YES
-                                                   target:self selector:@selector(dealSkewer1) animCompletion:nil];
-}
-
-- (int) pickDamage
+- (int) quickAttackDamage
 {
   float rand = (float)arc4random_uniform(RAND_MAX) / (float)RAND_MAX;
   if (rand < _chance){
     return _highDamage;
   }
   return _lowDamage;
-}
-
-- (void) dealSkewer1
-{
-  // Deal damage
-  [self.battleLayer dealDamage:[self pickDamage] enemyIsAttacker:(!self.belongsToPlayer) usingAbility:YES withTarget:self withSelector:@selector(dealSkewer2)];
-  
-  if (!self.belongsToPlayer) {
-    [self.battleLayer sendServerUpdatedValuesVerifyDamageDealt:NO];
-  }
-}
-
-- (void) dealSkewer2
-{
-  [self resetOrbCounter];
-  [self skillTriggerFinished:YES];
 }
 
 @end
