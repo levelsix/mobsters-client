74c658916565f91435544415ae12534c06ef8eb2
Rob Giusti <rockenstein.rock@gmail.com>
2015-02-24 15:30:00 -0800

    Thick skin fixes

--- Utopia/Classes/Battle/Skills/SkillThickSkin.m
+++ Utopia/Classes/Battle/Skills/SkillThickSkin.m
@@ -38,14 +38,6 @@
   return [NSSet setWithObjects:@(SideEffectTypeBuffThickSkin), nil];
 }
 
-- (void) restoreVisualsIfNeeded
-{
-  if ([self isActive])
-  {
-    [self addSkillSideEffectToSkillOwner:SideEffectTypeBuffThickSkin turnsAffected:self.turnsLeft];
-  }
-}
-
 -(NSInteger)modifyDamage:(NSInteger)damage forPlayer:(BOOL)player
 {
   if ([self isActive] && player != self.belongsToPlayer)
@@ -87,27 +79,6 @@
   return NO;
 }
 
-- (BOOL) onDurationStart
-{
-  [self addSkillSideEffectToSkillOwner:SideEffectTypeBuffThickSkin turnsAffected:self.turnsLeft];
-  
-  return NO;
-}
-
-- (BOOL) onDurationReset
-{
-  [self resetAfftectedTurnsCount:self.turnsLeft forSkillSideEffectOnSkillOwner:SideEffectTypeBuffThickSkin];
-  
-  return [super onDurationStart];
-}
-
-- (BOOL) onDurationEnd
-{
-  [self removeSkillSideEffectFromSkillOwner:SideEffectTypeBuffThickSkin];
-  
-  return [super onDurationEnd];
-}
-
 #pragma mark - Skill logic
 
 -(void)showDamageAbsorbed
@@ -118,34 +89,6 @@
                          bottomText:[NSString stringWithFormat:@"%ld DMG BLOCKED", (long)_damageAbsorbed]
                      withCompletion:^{}];
   }
-  
-  /*
-  // Display logo
-  CCSprite* logoSprite = [CCSprite spriteWithImageNamed:[self.skillImageNamePrefix stringByAppendingString:kSkillMiniLogoImageNameSuffix]];
-  logoSprite.position = CGPointMake((self.enemySprite.position.x + self.playerSprite.position.x) * .5f + self.playerSprite.contentSize.width * .5f - 10.f,
-                                    (self.playerSprite.position.y + self.enemySprite.position.y) * .5f + self.playerSprite.contentSize.height * .5f);
-  logoSprite.scale = 0.f;
-  [self.playerSprite.parent addChild:logoSprite z:50];
-  
-  // Display damage absorbed label
-  CCLabelTTF* floatingLabel = [CCLabelTTF labelWithString:[NSString stringWithFormat:@"%ld DAMAGE BLOCKED", (long)_damageAbsorbed] fontName:@"GothamNarrow-Ultra" fontSize:12];
-  floatingLabel.position = ccp(logoSprite.spriteFrame.rect.size.width * .5f, -13.f);
-  floatingLabel.fontColor = [CCColor colorWithRed:255.f / 225.f green:232.f / 225.f blue:174.f / 225.f];
-  floatingLabel.outlineColor = [CCColor colorWithRed:148.f / 225.f green:46.f / 225.f blue:11.f / 225.f];
-  floatingLabel.shadowOffset = ccp(0.f, -1.f);
-  floatingLabel.shadowColor = [CCColor colorWithWhite:0.f alpha:.75f];
-  floatingLabel.shadowBlurRadius = 2.f;
-  [logoSprite addChild:floatingLabel];
-  
-  // Animate both
-  [logoSprite runAction:[CCActionSequence actions:
-                         [CCActionDelay actionWithDuration:.3f],
-                         [CCActionEaseBounceOut actionWithAction:[CCActionScaleTo actionWithDuration:.5f scale:1.f]],
-                         [CCActionDelay actionWithDuration:.5f],
-                         [CCActionEaseIn actionWithAction:[CCActionScaleTo actionWithDuration:.3f scale:0.f]],
-                         [CCActionRemove action],
-                         nil]];
-   */
 }
 
 @end
