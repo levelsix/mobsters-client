e535b1ad781a72d9b319ddf6a09ce754fc0f4670
inlight2 <kenny@lvl6.com>
2015-02-27 17:35:36 -0800

    ui first steps

---
+++ Utopia/Classes/GameMenus/HomeMenu/Research/ResearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResearchViewController.h
+//  Utopia
+//
+//  Created by Kenneth Cox on 2/27/15.
+//  Copyright (c) 2015 LVL6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResearchViewController : UIViewController
+
+@end
---
+++ Utopia/Classes/GameMenus/HomeMenu/Research/ResearchViewController.m
@@ -0,0 +1,37 @@
+//
+//  ResearchViewController.m
+//  Utopia
+//
+//  Created by Kenneth Cox on 2/27/15.
+//  Copyright (c) 2015 LVL6. All rights reserved.
+//
+
+#import "ResearchViewController.h"
+
+@interface ResearchViewController ()
+
+@end
+
+@implementation ResearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
