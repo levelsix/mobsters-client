a8c48ad7bb68728062e05c075f81aad82a8f47b2
Behrouz Namakshenas <behrouz@lvl6.com>
2015-03-02 20:53:54 -0800

    Animated damage label no longer has a scale animation

--- Utopia/Classes/Battle/NewBattleLayer.m
+++ Utopia/Classes/Battle/NewBattleLayer.m
@@ -1102,13 +1102,14 @@
     const int   updateRepeatCount = ceilf(updateDuration / .05f);
     const float updateDamageIncrement = (initialDamage - modifiedDamage) / (float)updateRepeatCount;
     
-    const float labelScaleInitial = (initialDamage > modifiedDamage) ? 1.2f : 1.f;
-    const float labelScaleTarget  = (initialDamage > modifiedDamage) ? 1.f : 1.2f;
-    const int   labelScaleRepeatCount = ceilf(updateDuration / .5f);
-    const float labelScaleIncrement = SGN(labelScaleTarget - labelScaleInitial) * .05f;
+    const float labelScaleInitial = 1.f;
+//  const float labelScaleInitial = (initialDamage > modifiedDamage) ? 1.2f : 1.f;
+//  const float labelScaleTarget  = (initialDamage > modifiedDamage) ? 1.f : 1.2f;
+//  const int   labelScaleRepeatCount = ceilf(updateDuration / .5f);
+//  const float labelScaleIncrement = SGN(labelScaleTarget - labelScaleInitial) * .05f;
     
     __block float damage = initialDamage;
-    __block float scale = labelScaleInitial;
+//  __block float scale = labelScaleInitial;
     CCActionFiniteTime* labelUpdateAction = [CCActionSequence actions:
                                              [CCActionSpawn actions:
                                               [CCActionRepeat actionWithAction:
@@ -1121,6 +1122,7 @@
                                                                     updateDamageIncrement > 0 ? modifiedDamage : initialDamage));     // Lower limit
                                                  }],
                                                 [CCActionDelay actionWithDuration:.05f], nil] times:updateRepeatCount],
+                                              /*
                                               [CCActionRepeat actionWithAction:
                                                [CCActionSequence actions:
                                                 [CCActionCallBlock actionWithBlock: // Update label scale
@@ -1134,7 +1136,7 @@
                                                                 MAX(scale + labelScaleIncrement,
                                                                     labelScaleIncrement > 0 ? labelScaleInitial : labelScaleTarget)); // Lower limit
                                                  }],
-                                                [CCActionDelay actionWithDuration:.5f], nil] times:labelScaleRepeatCount], nil],
+                                                [CCActionDelay actionWithDuration:.5f], nil] times:labelScaleRepeatCount], */ nil],
                                              [CCActionCallBlock actionWithBlock:    // Set final damage number
                                               ^{
                                                 [damageLabel setString:[NSString stringWithFormat:@"%@", [Globals commafyNumber:modifiedDamage]]];
