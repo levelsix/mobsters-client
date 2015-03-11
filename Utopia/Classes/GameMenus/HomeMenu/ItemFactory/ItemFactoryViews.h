216e0eeb058ecf9b5dd10afa2e8bcdee4a504961
Ashwin Kamath <ashwin@lvl6.com>
2015-03-10 17:14:14 -0700

    bug fixes in mini job/building

---
+++ Utopia/Classes/GameMenus/HomeMenu/ItemFactory/ItemFactoryViews.h
@@ -0,0 +1,13 @@
+//
+//  ItemFactoryViews.h
+//  Utopia
+//
+//  Created by Ashwin Kamath on 3/10/15.
+//  Copyright (c) 2015 LVL6. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ItemFactoryViews : NSObject
+
+@end
---
+++ Utopia/Classes/GameMenus/HomeMenu/ItemFactory/ItemFactoryViews.m
@@ -0,0 +1,13 @@
+//
+//  ItemFactoryViews.m
+//  Utopia
+//
+//  Created by Ashwin Kamath on 3/10/15.
+//  Copyright (c) 2015 LVL6. All rights reserved.
+//
+
+#import "ItemFactoryViews.h"
+
+@implementation ItemFactoryViews
+
+@end
