66a255d399cc3747c58addaa74abc6c431b22cb5
Rob Giusti <rockenstein.rock@gmail.com>
2015-03-20 08:37:25 -0700

    All status ailments are now in past tense

--- Utopia/Classes/Battle/NewBattleLayer.m
+++ Utopia/Classes/Battle/NewBattleLayer.m
@@ -821,7 +821,7 @@
     
     if (self.myPlayerObject.isStunned)
     {
-      [[skillManager enemySkillControler] showSkillPopupAilmentOverlay:@"STUN" bottomText:@"TURN LOST"];
+      [[skillManager enemySkillControler] showSkillPopupAilmentOverlay:@"STUNNED" bottomText:@"TURN LOST"];
       [self endMyTurnAfterDelay:1.5f];
       return;
     }
@@ -882,7 +882,7 @@
             // If the enemy's stunned, short the attack function
             if (self.enemyPlayerObject.isStunned)
             {
-              [[skillManager playerSkillControler] showSkillPopupAilmentOverlay:@"STUN" bottomText:@"TURN LOST"];
+              [[skillManager playerSkillControler] showSkillPopupAilmentOverlay:@"STUNNED" bottomText:@"TURN LOST"];
               [self performAfterDelay:1.5 block:^{
                 [self endEnemyTurn];
               }];
@@ -895,7 +895,7 @@
             {
               self.enemyPlayerObject.isConfused = NO;
               
-              [[skillManager playerSkillControler] showSkillPopupAilmentOverlay:@"CONFUSION" bottomText:[NSString stringWithFormat:@"%i DMG TO SELF", _enemyDamageDealt]];
+              [[skillManager playerSkillControler] showSkillPopupAilmentOverlay:@"CONFUSED" bottomText:[NSString stringWithFormat:@"%i DMG TO SELF", _enemyDamageDealt]];
               
               CCSprite* confusedPopup = [CCSprite spriteWithImageNamed:@"confusionbubble.png"];
               [confusedPopup setAnchorPoint:CGPointMake(.5f, 0.f)];
@@ -979,7 +979,7 @@
     {
       self.myPlayerObject.isConfused = NO;
       
-      [[skillManager enemySkillControler] showSkillPopupAilmentOverlay:@"CONFUSION" bottomText:[NSString stringWithFormat:@"%i DMG TO SELF", _myDamageDealt]];
+      [[skillManager enemySkillControler] showSkillPopupAilmentOverlay:@"CONFUSED" bottomText:[NSString stringWithFormat:@"%i DMG TO SELF", _myDamageDealt]];
       
       CCSprite* confusedPopup = [CCSprite spriteWithImageNamed:@"confusionbubble.png"];
       [confusedPopup setAnchorPoint:CGPointMake(.5f, 0.f)];
