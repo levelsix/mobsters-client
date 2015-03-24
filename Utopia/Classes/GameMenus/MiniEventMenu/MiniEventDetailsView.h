43726126798377792ba05eb3a8cadf1bdce4f726
Behrouz Namakshenas <behrouz@lvl6.com>
2015-03-24 13:39:03 -0700

    Mini events initial commit

---
+++ Utopia/Classes/GameMenus/MiniEventMenu/MiniEventDetailsView.h
@@ -0,0 +1,13 @@
+//
+//  MiniEventDetailsView.h
+//  Utopia
+//
+//  Created by Behrouz Namakshenas on 3/24/15.
+//  Copyright (c) 2015 LVL6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MiniEventDetailsView : UIView
+
+@end
---
+++ Utopia/Classes/GameMenus/MiniEventMenu/MiniEventDetailsView.m
@@ -0,0 +1,13 @@
+//
+//  MiniEventDetailsView.m
+//  Utopia
+//
+//  Created by Behrouz Namakshenas on 3/24/15.
+//  Copyright (c) 2015 LVL6. All rights reserved.
+//
+
+#import "MiniEventDetailsView.h"
+
+@implementation MiniEventDetailsView
+
+@end
