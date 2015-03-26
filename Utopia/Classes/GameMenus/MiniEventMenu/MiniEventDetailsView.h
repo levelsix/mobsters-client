eca0b6198db5665c9216eef9bd87e4612c6c174e
Behrouz Namakshenas <behrouz@lvl6.com>
2015-03-25 17:46:15 -0700

    Mini events detail view

--- Utopia/Classes/GameMenus/MiniEventMenu/MiniEventDetailsView.h
+++ Utopia/Classes/GameMenus/MiniEventMenu/MiniEventDetailsView.h
@@ -11,18 +11,19 @@
 
 @class THLabel;
 @class SplitImageProgressBar;
+@class MiniEventTierPrizeView;
 
 @interface MiniEventDetailsView : UIView <MiniEventInfoViewProtocol>
 
-@property (nonatomic, retain) IBOutlet UIImageView* eventInfoBackground;          // Static
+@property (nonatomic, retain) IBOutlet UIImageView* eventInfoBackground;
 @property (nonatomic, retain) IBOutlet UIImageView* eventInfoImage;
 @property (nonatomic, retain) IBOutlet THLabel* eventInfoName;
 @property (nonatomic, retain) IBOutlet THLabel* eventInfoDesc;
-@property (nonatomic, retain) IBOutlet THLabel* eventInfoEndsIn;                  // Static
+@property (nonatomic, retain) IBOutlet THLabel* eventInfoEndsIn;
 @property (nonatomic, retain) IBOutlet THLabel* eventInfoTimeLeft;
-@property (nonatomic, retain) IBOutlet THLabel* eventInfoMyPoints;                // Static
+@property (nonatomic, retain) IBOutlet THLabel* eventInfoMyPoints;
 @property (nonatomic, retain) IBOutlet THLabel* eventInfoPointsEearned;
-@property (nonatomic, retain) IBOutlet UIImageView* progressBarBackground;        // Static
+@property (nonatomic, retain) IBOutlet UIImageView* progressBarBackground;
 @property (nonatomic, retain) IBOutlet SplitImageProgressBar* pointsProgressBar;
 @property (nonatomic, retain) IBOutlet UIImageView* tier1IndicatorArrow;
 @property (nonatomic, retain) IBOutlet UILabel*     tier1IndicatorLabel;
@@ -32,5 +33,8 @@
 @property (nonatomic, retain) IBOutlet UILabel*     tier3IndicatorLabel;
 @property (nonatomic, retain) IBOutlet UIView*  pointCounterView;
 @property (nonatomic, retain) IBOutlet UILabel* pointCounterLabel;
+@property (nonatomic, retain) IBOutlet MiniEventTierPrizeView* tier1PrizeView;
+@property (nonatomic, retain) IBOutlet MiniEventTierPrizeView* tier2PrizeView;
+@property (nonatomic, retain) IBOutlet MiniEventTierPrizeView* tier3PrizeView;
 
 @end
---
+++ Utopia/Classes/GameMenus/MiniEventMenu/MiniEventTierPrizeCell.m
@@ -0,0 +1,18 @@
+//
+//  MiniEventTierPrizeCell.m
+//  Utopia
+//
+//  Created by Behrouz Namakshenas on 3/25/15.
+//  Copyright (c) 2015 LVL6. All rights reserved.
+//
+
+#import "MiniEventTierPrizeCell.h"
+
+@implementation MiniEventTierPrizeCell
+
+- (void) awakeFromNib
+{
+  [super awakeFromNib];
+}
+
+@end
