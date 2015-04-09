ef17df9c62cc20627ead21ca52eb8427a8a318c9
inlight2 <kenny@lvl6.com>
2015-04-09 16:45:17 -0700

    proto update

---
+++ Utopia/Classes/GameMenus/ItemSelectMenu/MiniJobRefreshItemsFiller.h
@@ -0,0 +1,13 @@
+//
+//  MiniJobRefreshItemsFiller.h
+//  Utopia
+//
+//  Created by Kenneth Cox on 4/9/15.
+//  Copyright (c) 2015 LVL6. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MiniJobRefreshItemsFiller : NSObject
+
+@end
---
+++ Utopia/Classes/GameMenus/ItemSelectMenu/MiniJobRefreshItemsFiller.m
@@ -0,0 +1,13 @@
+//
+//  MiniJobRefreshItemsFiller.m
+//  Utopia
+//
+//  Created by Kenneth Cox on 4/9/15.
+//  Copyright (c) 2015 LVL6. All rights reserved.
+//
+
+#import "MiniJobRefreshItemsFiller.h"
+
+@implementation MiniJobRefreshItemsFiller
+
+@end
