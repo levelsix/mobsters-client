091d45b32977e4e83d1221dad1cfb67828417858
Ashwin Kamath <ashwin@lvl6.com>
2015-04-14 15:37:27 -0700

    switching branches

---
+++ Utopia/Classes/GameMenus/ShopMenu/Sales/GemPackageViewController.h
@@ -0,0 +1,13 @@
+//
+//  GemPackageViewController.h
+//  Utopia
+//
+//  Created by Ashwin Kamath on 4/14/15.
+//  Copyright (c) 2015 LVL6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GemPackageViewController : UIViewController
+
+@end
---
+++ Utopia/Classes/GameMenus/ShopMenu/Sales/GemPackageViewController.m
@@ -0,0 +1,37 @@
+//
+//  GemPackageViewController.m
+//  Utopia
+//
+//  Created by Ashwin Kamath on 4/14/15.
+//  Copyright (c) 2015 LVL6. All rights reserved.
+//
+
+#import "GemPackageViewController.h"
+
+@interface GemPackageViewController ()
+
+@end
+
+@implementation GemPackageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
