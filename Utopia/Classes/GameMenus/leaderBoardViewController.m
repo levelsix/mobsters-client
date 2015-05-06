923246bc1f36be514b689cabfb64751832f90dbb
inlight2 <kenny@lvl6.com>
2015-05-06 14:56:01 -0700

    base controller created

---
+++ Utopia/Classes/GameMenus/leaderBoardViewController.m
@@ -0,0 +1,13 @@
+//
+//  leaderBoardViewController.m
+//  Utopia
+//
+//  Created by Kenneth Cox on 5/5/15.
+//  Copyright (c) 2015 LVL6. All rights reserved.
+//
+
+#import "leaderBoardViewController.h"
+
+@implementation leaderBoardViewController
+
+@end
