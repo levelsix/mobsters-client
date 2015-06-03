fcbefebd0a828135f4352bf1d9f3f576a697fab1
Ashwin Kamath <ashwin@lvl6.com>
2015-06-02 19:36:37 -0700

    response caching

---
+++ Pods/Target Support Files/Pods-Mobsters-SocketRocket/Pods-Mobsters-SocketRocket-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mobsters_SocketRocket : NSObject
+@end
+@implementation PodsDummy_Pods_Mobsters_SocketRocket
+@end
--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -96,6 +96,12 @@
 #define COCOAPODS_VERSION_MINOR_Reachability 2
 #define COCOAPODS_VERSION_PATCH_Reachability 0
 
+// SocketRocket
+#define COCOAPODS_POD_AVAILABLE_SocketRocket
+#define COCOAPODS_VERSION_MAJOR_SocketRocket 0
+#define COCOAPODS_VERSION_MINOR_SocketRocket 2
+#define COCOAPODS_VERSION_PATCH_SocketRocket 0
+
 // UIImage+BlurredFrame
 #define COCOAPODS_POD_AVAILABLE_UIImage_BlurredFrame
 #define COCOAPODS_VERSION_MAJOR_UIImage_BlurredFrame 0
